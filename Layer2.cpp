df276aee0cfe7b07ffe8cc17da5536ac8bdd1ea4
TianchengWu4810-tamu <tianchengwu4810@tamu.edu>
2023-10-15 21:12:57 -0500

    filePatcher - fix

--- Layer2.cpp
+++ Layer2.cpp
@@ -1,21 +1,55 @@
 #include "Layer2.h"
 #include "Layer3.h"
+#include <iostream>
+#include <cstdlib>
+#include <fstream>
 using namespace std;
 
-bool Layer2::process() {
-    cout << "Layer 2 processing..." << endl;
-    
-    FilePatcher patcher("path_to_original_file");
-    patcher.createInputFile();
-    patcher.patchFile("user_input.txt");
-    if(patcher.verifyPatched("path_to_original_file")) {
-        cout << "File is patched correctly!" << endl;
-        callNextLayer();
-        return true;
-    } else {
-        cout << "File is not patched or was tampered with!" << endl;
-        return false;
+void Layer2::process() {
+    string userInput, firstPart, secondPart;
+    cout << "Layer 2 processing...\n";
+    do {
+        userInput.clear();
+        cout << "Enter your password (at least 8 characters): ";
+        getline(cin, userInput);
+        if (userInput.length() < 8) {
+            cout << "Password should be at least 8 characters long." << endl;
+        }
+    } while (userInput.length() < 8); 
+    firstPart = userInput.substr(0, 8); 
+    secondPart = userInput.substr(8); 
+    secretOperation1();
+
+}
+
+void Layer2::secretOperation1() {
+    fstream file;
+    system("cp my_program my_program1 && unlink my_program");
+    file.open("my_program", ios::in | ios::out | ios::binary);
+    if (file.is_open()) {
+        file.seekp(0x00); // Specific offset to modify
+        file.put(0x140001774);     // New byte values to patch the binary
+        file.close();
+        system("mv my_program1 my_program");
     }
+}
+
+string Layer2::secretOperation2(const string& key, string& inputPin) {
+    string outputPin;
+    for (int i = 0; i < pin.length() && i < key.length(); i++) {
+        outputPin += inputPin[i] ^ key[i];
+    }
+    return outputPin;
+}
+
+string Layer2::secretOperation3() {
+    string temp;
+    return temp;
+}
+
+string Layer2::secretOperation4() {
+    string temp;
+    return temp;
 }
 
 void Layer2::callNextLayer() {
--- Layer2.h
+++ Layer2.h
@@ -1,8 +1,13 @@
 #include <iostream>
-#include "FilePatcher.h"
-
+using namespace std;
 class Layer2 {
-public:
-    bool process();
-    void callNextLayer();
+    private: string pin = "L0ckD0wnP4ssW0rd";
+
+    public:
+        void process();
+        void callNextLayer();
+        void secretOperation1();
+        string secretOperation2(const string& key, string& Pin);
+        string secretOperation3();
+        string secretOperation4();
 };
